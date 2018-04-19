66bcf45f241b85109700ded04c19b6f4bf09d5b4
srushtikotak <srushti.kotak96@gmail.com>
2018-04-19 17:18:33 +0530

    Initial Commit

--- app/routes.py
+++ app/routes.py
@@ -62,3 +62,24 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
+
+'''
+CREATE TABLE `todolist` (
+`id` int(11) NOT NULL,
+`restaurant_name` varchar(128) DEFAULT NULL,
+`location` varchar(128) DEFAULT NULL,
+`zomato_rating` float(12) DEFAULT NULL,
+`average_cost` double(10000) DEFAULT NULL,
+PRIMARY KEY (`id`));
+
+INSERT INTO todolist(id,restaurant_name,location,zomato_rating,average_cost) VALUES (1, 'Skky Ramada', 'Powai', 4.1, 3000 );
+INSERT INTO todolist(id,restaurant_name,location,zomato_rating,average_cost) VALUES (2, 'Bayview Cafe', 'Coloba', 3.7, 1200 );
+INSERT INTO todolist(id,restaurant_name,location,zomato_rating,average_cost) VALUES (3, 'Light House Cafe', 'Worli', 4.2, 1200 );
+INSERT INTO todolist(id,restaurant_name,location,zomato_rating,average_cost) VALUES (4, 'Imperial China', 'Chakala', 4.5, 3500 );
+INSERT INTO todolist(id,restaurant_name,location,zomato_rating,average_cost) VALUES (5, 'Tamasha', 'Lower Parel', 4.2, 1800 );
+INSERT INTO todolist(id,restaurant_name,location,zomato_rating,average_cost) VALUES (6, 'Ark - Deck Bar', 'Bandra', 4.0, 6000 );
+INSERT INTO todolist(id,restaurant_name,location,zomato_rating,average_cost) VALUES (7, 'Marina', 'Colaba', 3.5, 1600 );
+INSERT INTO todolist(id,restaurant_name,location,zomato_rating,average_cost) VALUES (8, 'SpiceKlub', 'Lower Parel', 4.5, 2100 );
+INSERT INTO todolist(id,restaurant_name,location,zomato_rating,average_cost) VALUES(9,'Dome-InterContinental','Churgegate',4.1,3500);
+INSERT INTO todolist(id,restaurant_name,location,zomato_rating,average_cost) VALUES (10, 'Aer - 4 seasons', 'Worli',4.3,4000);
+'''
